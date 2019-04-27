6589da8d30d40bfe96460af73215026d2c4af57e
ChildhoodDPC <33065086+ChildhoodDPC@users.noreply.github.com>
2019-04-27 15:53:55 -0400

    Update test.php

--- chirper/test.php
+++ chirper/test.php
@@ -1,3 +1,8 @@
 <?php
-  echo "Test message";
+  session_start();
+  if(!isset($_SESSION['count'])){
+    $_SESSION['count'] = 0;
+  }
+   $_SESSION['count']++;
+  echo "Test message". $_SESSION['count'];
 ?>
