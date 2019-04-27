6911b87ec0157b147d9bd79b0f6255b5a749949d
ChildhoodDPC <33065086+ChildhoodDPC@users.noreply.github.com>
2019-04-27 17:19:19 -0400

    Update dbconnectbo.php

--- dbconnectbo.php
+++ dbconnectbo.php
@@ -1,6 +1,6 @@
 <?php
-        $h = 'ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com ';
-        $u = '  fbmqmr21m3u9teq9';
+        $h = 'ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        $u = 'fbmqmr21m3u9teq9';
         $p = 'kjwvt8ge5h8vspxp';
         $dbname = 'umxeumhyjae6jbrf';
         $db = mysqli_connect($h,$u,$p,$dbname);
