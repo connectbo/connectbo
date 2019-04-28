552c1e54d4a29d482ba2f93db67312bb3ff8ff31
ChildhoodDPC <33065086+ChildhoodDPC@users.noreply.github.com>
2019-04-28 00:07:42 -0400

    Delete test.php

--- chirper/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-  session_start();
-  require "/fs1/home/bo7/dbconnect.php";
-  if(!isset($_SESSION['count'])){
-    $_SESSION['count'] = 0;
-  }
-   $_SESSION['count']++;
-  echo "Test message". $_SESSION['count'];
-?>
