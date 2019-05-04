785399e0472e6fbc82c0ca7a541bb2fffb29fa9f
ChildhoodDPC <33065086+ChildhoodDPC@users.noreply.github.com>
2019-05-03 21:17:20 -0400

    Add files via upload

--- chirper/home.php
+++ chirper/home.php
@@ -158,7 +158,7 @@
                         </li>
                         <li class="list-group-item">
                             <div class="h6 text-muted">Following</div>
-                            <div id="divhiddenid" style="display: none;" value="<?php echo $_SESSION['userid'];?>"></div>
+                            <div id="divhiddenid" style="display: none;" value='<?php echo $_SESSION['userid'];?>'><?php echo $_SESSION['userid'];?></div>
                             <div class="h5"><?php echo $_SESSION['following'];?></div>
                         </li>
                     </ul>
