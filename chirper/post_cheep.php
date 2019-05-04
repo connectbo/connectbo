6595f609ca1743e88ab460fb6ff2ddc18bddd1ea
ChildhoodDPC <33065086+ChildhoodDPC@users.noreply.github.com>
2019-05-03 20:48:20 -0400

    Update post_cheep.php

--- chirper/post_cheep.php
+++ chirper/post_cheep.php
@@ -3,7 +3,7 @@
 	session_start();
 	require "../dbconnectbo.php";
 		if(isset($_POST['text'])){
-			$_SESSION['postbox'] = mysqli_real_escape_string($db,$_POST['text']);
+			$postbox = mysqli_real_escape_string($db,$_POST['text']);
 			$userid = mysqli_real_escape_string($db,$_SESSION['userid']);
 			$time = date('Y-m-d h:m:s',time());
 			$insert_query = "insert into cheeps (cheep_text, created_date, user_id) VALUES ('".$postbox."','".$time."','".$userid."')";
