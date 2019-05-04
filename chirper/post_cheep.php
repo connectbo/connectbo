d176b643686a03fc31d44e34d01cd32884bb49cb
ChildhoodDPC <33065086+ChildhoodDPC@users.noreply.github.com>
2019-05-03 20:54:25 -0400

    Add files via upload

--- chirper/post_cheep.php
+++ chirper/post_cheep.php
@@ -1,8 +1,8 @@
 <?php
 	date_default_timezone_set("EST"); 
-	session_start();
-	require "../dbconnectbo.php";
 		if(isset($_POST['text'])){
+			session_start();
+			require "../dbconnectbo.php";
 			$postbox = mysqli_real_escape_string($db,$_POST['text']);
 			$userid = mysqli_real_escape_string($db,$_SESSION['userid']);
 			$time = date('Y-m-d h:m:s',time());
