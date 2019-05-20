ac8d6a2694b3a27528d01ea6dd5a2ea87d59e7f4
ChildhoodDPC <33065086+ChildhoodDPC@users.noreply.github.com>
2019-05-19 23:59:27 -0400

    Add files via upload

---
+++ chirper/chirper-admin/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule
+} from '@angular/material';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ chirper/chirper-admin/src/app/cheeps.ts
@@ -0,0 +1,7 @@
+export class Cheep {
+  constructor(
+    public id: number,
+    public text: string,
+    public date: number,
+    public username: String ) {}
+}
