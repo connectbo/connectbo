b3313fae5452c83a7194af0316b09cb5fa97446b
Bo <bo7@live.unc.edu>
2019-06-13 18:58:04 -0400

    Update home.php

--- chirper/home.php
+++ chirper/home.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+<title>Chirper@UNC</title>
 <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
