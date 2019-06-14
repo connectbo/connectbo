dae71fc2f5a904288dd98894b257d5f2e821f051
Bo <bo7@live.unc.edu>
2019-06-14 01:06:57 -0400

    Update home.php

--- chirper/home.php
+++ chirper/home.php
@@ -276,8 +276,8 @@
                         <div class="card-body">
                             <h5 class="card-title">Let us celebrate with Meng Li's birthday!</h5>
                             <h6 class="card-subtitle mb-2 text-muted">世界上无敌可爱的兔子蒙蒙</h6>
-                            <p class="card-text">北卡最会穿衣打扮的性感女神 学习又好</p>
-                            <a href="https://www.linkedin.com/in/bo-zhou-a64a5b122/" class="card-link">如有需求请联系她的经纪人 Dr. Zhou 谢谢合作！</a><br />
+                            <p class="card-text">北卡最会穿衣打扮的殿堂级性感校花</p>
+                            <a href="https://www.linkedin.com/in/bo-zhou-a64a5b122/" class="card-link">如有需求请联系独家经纪人 Dr. Zhou 谢谢合作！</a><br />
                         </div>
                     </div>
                 </div>
