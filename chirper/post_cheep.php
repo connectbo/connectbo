c054959f3ad3fdef96d6f92b713b2df7b7ffa7ab
Bo <bo7@live.unc.edu>
2019-06-14 01:15:45 -0400

    Update post_cheep.php

--- chirper/post_cheep.php
+++ chirper/post_cheep.php
@@ -4,7 +4,7 @@
 			require "../dbconnectbo.php";
 			$postbox = mysqli_real_escape_string($db,$_POST['text']);
 			$userid = mysqli_real_escape_string($db,$_POST['userid']);
-			$time = date('Y-m-d H:i:s',time());
+			$time = date('Y-m-d h:i:s',time());
 			$insert_query = "insert into cheeps (cheep_text, created_date, user_id) VALUES ('".$postbox."','".$time."','".$userid."')";
 			if($result= mysqli_query($db,$insert_query)){
 				echo "Cheep Posted Successfully!";
