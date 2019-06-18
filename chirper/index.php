18a814c4c0b71a3afe9c374ff94c88165ed9efe3
Bo <bo7@live.unc.edu>
2019-06-17 22:35:13 -0400

    Update index.php

--- chirper/index.php
+++ chirper/index.php
@@ -22,12 +22,12 @@
                 $login_query = "select * from users where username='" . $_POST['inputUsername'] . "'";
                 if ($result = mysqli_query($db, $login_query)) {
                     while ($row = mysqli_fetch_row($result)) {
-                        if (sha1($_POST['inputPassword']) == $row['4']) {
-                            $_SESSION['firstname'] = $row['2'];
-                            $_SESSION['lastname'] = $row['3'];
-                            $_SESSION['userid'] = $row['0'];
-                            $_SESSION['usertype'] = $row['5'];
-                            $_SESSION['username'] = $row['1'];
+                        if (sha1($_POST['inputPassword']) == $row[4]) {
+                            $_SESSION['firstname'] = $row[2];
+                            $_SESSION['lastname'] = $row[3];
+                            $_SESSION['userid'] = $row[0];
+                            $_SESSION['usertype'] = $row[5];
+                            $_SESSION['username'] = $row[1];
                         }
                     }
                 }
